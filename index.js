0064fb45de5f8e0d8199a735a0eb1f95308ed79c
rosekamau <kamaurose236@gmail.com>
2024-07-01 11:17:42 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 =31;
+let num2 =2;
+let multiply =num1 * num2;
 
-// Write your code here
+let random =(Math.random() >0)+1
+
+let num3 =24;
+let num4 =5;
+let mod = num3 % num4;
+
+let max =(Math.max(1, 7, 15, 20))
+
